b79b801c71744aef0d3dd6499a9ad1fa79cb226f
Tong <czt_work@163.com>
2016-11-08 17:27:14 +0800

    添加注释

--- CTJSONToModelTemplateDemo/CTJSONToModelTemplate/ViewController.m
+++ CTJSONToModelTemplateDemo/CTJSONToModelTemplate/ViewController.m
@@ -23,8 +23,10 @@
     
     NSString * path = [[NSBundle mainBundle] pathForResource:@"testData" ofType:@"json"];
     id jsonData = [NSJSONSerialization JSONObjectWithData:[[NSData alloc] initWithContentsOfFile:path] options:NSJSONReadingMutableContainers error:nil];
-//    [NSObject autoProduceModelTemplateWithData:jsonData rootModel:@"Data"];
+//  1. 将控制台打印出来的代码分别复制到 创建的Modle 的 .h 和 .m  文件中
+    [NSObject autoProduceModelTemplateWithData:jsonData rootModel:@"Data"];
     
+//   2. 使用MJExtension将后台返回的数据转换成Model
     DataModel * model = [DataModel mj_objectWithKeyValues:jsonData];
     
     NSLog(@"%@", model.body.aboutCoopH5Url);
